e0933e969d59efbacbffb69748658252585ef091
Chamuditha Prabhath Jayasena <36603594+chamuditha4@users.noreply.github.com>
2023-10-16 20:49:10 +0530

    small changes

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+# Read the contents of your README.md file as the long description
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='midjpy',
-    version='1.0.0',
+    version='1.0.2',
     description='Python client for the MIDJ API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify the content type
     author='dmaxxy',
     author_email='dmaxxy.bhw@gmail.com',
     packages=['midjpy'],
